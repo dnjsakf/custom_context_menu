98d28da35dcc66ef3c0772e0250576b813ad1856
heoDolf <dnjsakf@gmail.com>
2020-06-02 23:59:52 +0900

    added 'closest' polyfill for ie9+

--- custom_context_menu.js
+++ custom_context_menu.js
@@ -1,3 +1,16 @@
+/* Polyfill */
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function(s) {
+    var el = this;
+
+    do {
+      if (el.matches(s)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+  };
+}
+
 function Functions(){}
 
 Functions.prototype = (function(){
